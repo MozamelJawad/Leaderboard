1a2c37ecf10d69d9b4b3ec828d875a4e007b218e
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-02 19:41:31 +0430

    solve js linter errors

--- src/modules/recentScore.js
+++ src/modules/recentScore.js
@@ -1,36 +1,36 @@
 const scores = [
-    {
-        id: 1,
-        name: "Ahmad",
-        mark: 100
-    },
-    {
-        id: 2,
-        name: "Jawad",
-        mark: 98
-    },
-    {
-        id: 3,
-        name: "Modasir",
-        mark: 90
-    },
-    {
-        id: 4,
-        name: "Bashar Ahmad",
-        mark: 95
-    },
-    {
-        id: 5,
-        name: "Shaheer Ahmad",
-        mark: 90
-    },
+  {
+    id: 1,
+    name: 'Ahmad',
+    mark: 100,
+  },
+  {
+    id: 2,
+    name: 'Jawad',
+    mark: 98,
+  },
+  {
+    id: 3,
+    name: 'Modasir',
+    mark: 90,
+  },
+  {
+    id: 4,
+    name: 'Bashar Ahmad',
+    mark: 95,
+  },
+  {
+    id: 5,
+    name: 'Shaheer Ahmad',
+    mark: 90,
+  },
 ];
 
 scores.forEach((score) => {
-    const scoreContainer = document.querySelector('.list');
-    const markDiv = document.createElement('li');
-    markDiv.innerHTML = `
+  const scoreContainer = document.querySelector('.list');
+  const markDiv = document.createElement('li');
+  markDiv.innerHTML = `
             ${score.name}: ${score.mark}
            `;
-    scoreContainer.append(markDiv);
+  scoreContainer.append(markDiv);
 });
