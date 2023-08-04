dd55172cfbbd8750e6a9d8e31d349c3baa661086
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-04 15:56:22 +0430

    import functions for add and display

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
 import './style.css';
-import './modules/recentScore.js';
+import {addScore, displayList, url} from "./modules/api.js";
+
+const form = document.querySelector('#form');
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+const refreshBtn = document.querySelector('#refreshBtn');
+
+
+const AddNameScore = async (event) => {
+    event.preventDefault();
+    if (!name.value || !score.value) return;
+    await addScore({ user: name.value, score: score.value });
+    name.value = '';
+    score.value = '';
+};
+
+const displayNameScore = async () => {
+    const fetchResult = await fetch(url);
+    const data = await fetchResult.json();
+    displayList(data.result);
+};
+
+displayNameScore();
+
+form.addEventListener('submit', AddNameScore);
+refreshBtn.addEventListener('click', displayNameScore);
+
+
+
+
+
+
+
